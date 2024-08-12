33a112d745c6ca38fb103dd505e523374a5d9438
Bevan Hunt <bevan@bevanhunt.com>
2024-08-12 13:03:17 -0700

    delete example apps

--- fastapi_auth_466/fastapi_auth_466/user.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from passlib.hash import pbkdf2_sha256
-
-class User:
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.hashed_password = pbkdf2_sha256.hash(password)
